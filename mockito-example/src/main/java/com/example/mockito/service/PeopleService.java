de4dc382cc935288e3229710601d591125c02fa0
pppurple <shuggie.otis0@gmail.com>
2016-10-24 03:20:23 +0900

    add Mockito test examples

---
+++ mockito-example/src/main/java/com/example/mockito/service/PeopleService.java
@@ -0,0 +1,67 @@
+package com.example.mockito.service;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Service
+public class PeopleService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    static private final String HOST = "http://dummy-host.com:8080";
+
+    public People getPeople() {
+        String url = HOST + "/api/people";
+        return restTemplate.getForObject(url, People.class);
+    }
+
+    public int postPeople(People people) {
+        String url = HOST + "/api/people";
+        return restTemplate.postForObject(url, people, Integer.class);
+    }
+
+    public int putPeople(People people) throws URISyntaxException {
+        String url = HOST + "/api/people";
+
+        URI uri = new URI(url);
+        RequestEntity<People> requestEntity = RequestEntity
+                .put(uri)
+                .body(people);
+
+        ResponseEntity<Integer> result = restTemplate.exchange(requestEntity, Integer.class);
+        return result.getBody();
+    }
+
+    public int deletePeople(String country) {
+        String url = HOST + "/api/people/" + country;
+
+        ResponseEntity<Integer> result = restTemplate.exchange(url,
+                HttpMethod.DELETE,
+                HttpEntity.EMPTY,
+                Integer.class,
+                country);
+
+        return result.getBody();
+    }
+
+    @Data
+    @NoArgsConstructor
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    public static class People {
+        String country;
+        int year;
+        int population;
+    }
+}
---
+++ mockito-example/src/test/java/com/example/mockito/service/PeopleServiceTest.java
@@ -0,0 +1,85 @@
+package com.example.mockito.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URISyntaxException;
+
+import static com.example.mockito.service.PeopleService.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class PeopleServiceTest {
+    @InjectMocks
+    PeopleService peopleService;
+
+    private PeopleService mockService;
+
+    @Before
+    public void before() {
+        mockService = mock(PeopleService.class);
+    }
+
+    @Test
+    public void getPeopleTest() {
+        People expected = new People();
+        expected.setCountry("japan");
+        expected.setYear(2001);
+        expected.setPopulation(1_000_000);
+
+        when(mockService.getPeople()).thenReturn(expected);
+
+        // assert
+        assertThat(mockService.getPeople().getCountry()).isEqualTo("japan");
+        assertThat(mockService.getPeople().getYear()).isEqualTo(2001);
+        assertThat(mockService.getPeople().getPopulation()).isEqualTo(1_000_000);
+
+        // verify
+        verify(mockService, times(3)).getPeople();
+        verify(mockService, never()).postPeople(new People());
+
+        // 前置記法
+        reset(mockService);
+        doReturn(expected).when(mockService).getPeople();
+        assertThat(mockService.getPeople().getCountry()).isEqualTo("japan");
+        assertThat(mockService.getPeople().getYear()).isEqualTo(2001);
+        assertThat(mockService.getPeople().getPopulation()).isEqualTo(1_000_000);
+    }
+
+    @Test
+    public void postPeopleTest() {
+        People people = new People();
+        people.setCountry("japan");
+        people.setYear(2001);
+        people.setPopulation(1_000_000);
+
+        when(mockService.postPeople(people)).thenReturn(1);
+
+        assertThat(mockService.postPeople(people)).isEqualTo(1);
+    }
+
+    @Test
+    public void putPeopleTest() throws URISyntaxException {
+        People people = new People();
+        people.setCountry("japan");
+        people.setYear(2001);
+        people.setPopulation(1_000_000);
+
+        when(mockService.putPeople(people)).thenReturn(1);
+
+        assertThat(mockService.putPeople(people)).isEqualTo(1);
+    }
+
+    @Test
+    public void deletePeopleTest() {
+        when(mockService.deletePeople("japan")).thenReturn(1);
+
+        assertThat(mockService.deletePeople("japan")).isEqualTo(1);
+    }
+}
