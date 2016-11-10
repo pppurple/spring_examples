ed4cace3d3e52594bdf4d3bba54cb3c4da217a23
purple <shuggie.otis0@gmail.com>
2016-11-11 02:13:12 +0900

    initial commit spring-aop-example

---
+++ spring-aop-example/src/main/java/spring/aop/com/example/SpringAopApplication.java
@@ -0,0 +1,12 @@
+package spring.aop.com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopApplication.class, args);
+	}
+}
