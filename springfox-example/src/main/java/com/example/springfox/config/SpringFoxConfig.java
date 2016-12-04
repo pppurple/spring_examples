a2aba00e0d0f150cae8de6d592afc88cfc5e1c35
purple <shuggie.otis0@gmail.com>
2016-12-05 03:02:38 +0900

    update

--- springfox-example/src/main/java/com/example/springfox/config/SpringFoxConfig.java
+++ springfox-example/src/main/java/com/example/springfox/config/SpringFoxConfig.java
@@ -1,35 +1,61 @@
 package com.example.springfox.config;
 
+import com.example.springfox.controller.CountryController;
+import com.fasterxml.classmate.TypeResolver;
 import com.google.common.base.Predicate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRule;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Date;
+import java.util.List;
+
 import static com.google.common.base.Predicates.or;
 import static springfox.documentation.builders.PathSelectors.regex;
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
+import com.example.springfox.controller.CountryController.MyResponseEntity;
+import com.example.springfox.controller.CountryController.MyResponseEntityWithStatus;
+import com.example.springfox.controller.CountryController.Country;
 
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+    @Autowired
+    private TypeResolver typeResolver;
+
     @Bean
     public Docket springFoxExampleDoc() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                     .apis(RequestHandlerSelectors.any())
-//                    .paths(PathSelectors.any())
                     .paths(paths())
                     .build()
                 .pathMapping("/")
+                .directModelSubstitute(Date.class, String.class)
+                .genericModelSubstitutes(MyResponseEntityWithStatus.class)
+                .alternateTypeRules(
+                        newRule(typeResolver.resolve(MyResponseEntity.class,
+                                    typeResolver.resolve(List.class, Country.class)),
+                                typeResolver.resolve(Country.class))
+                )
+                .useDefaultResponseMessages(false)
+                .enableUrlTemplating(true)
                 .apiInfo(apiInfo());
     }
+
 
     @Bean
     UiConfiguration uiConfiguration() {
@@ -48,7 +74,8 @@ public class SpringFoxConfig {
 
     private Predicate<String> paths() {
         return or(
-                regex("/api/people.*")
+                regex("/api/people.*"),
+                regex("/api/country.*")
         );
     }
 }
---
+++ springfox-example/src/main/java/com/example/springfox/controller/CountryController.java
@@ -0,0 +1,80 @@
+package com.example.springfox.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class CountryController {
+    @ResponseBody
+    @RequestMapping(value = "/api/country/now", method = RequestMethod.GET)
+    public Date getDate() {
+        return new Date();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/api/country/{countryName}/{cityName}", method = RequestMethod.GET)
+    public MyResponseEntityWithStatus<City> getCity(@PathVariable String country, @PathVariable String city) {
+        City awa = new City("awa", 100);
+        HttpStatus status = HttpStatus.OK;
+        return new MyResponseEntityWithStatus<>(awa, status);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/api/country/{countryName}", method = RequestMethod.GET)
+    public MyResponseEntity<List<Country>> getCountry(@PathVariable String country) {
+        Country america = new Country("America", "English", 1_000_000_000);
+        Country japan = new Country("Japan", "japanese", 1_000_000);
+        List<Country> countries = Arrays.asList(america, japan);
+        return new MyResponseEntity<>(countries);
+    }
+
+    @ApiIgnore
+    @ResponseBody
+    @RequestMapping(value = "/api/country/dummy", method = RequestMethod.GET)
+    public String dummy() {
+        return "dummy country";
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class Country {
+        String name;
+        String language;
+        int population;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class City {
+        String name;
+        int population;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class MyResponseEntity<T> {
+        T res;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class MyResponseEntityWithStatus<T> {
+        T res;
+        HttpStatus httpStatus;
+    }
+}
