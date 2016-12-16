de49125875e06aaa18dbd12d2d48fcb4ebfe9044
pppurple <shuggie.otis0@gmail.com>
2016-12-16 19:54:20 +0900

    add initial files

---
+++ not-recommend-field-injection/src/main/java/not/recommend/field/injection/example/NotRecommendFieldInjectionApplication.java
@@ -0,0 +1,12 @@
+package not.recommend.field.injection.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotRecommendFieldInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotRecommendFieldInjectionApplication.class, args);
+	}
+}
---
+++ not-recommend-field-injection/src/test/java/not/recommend/field/injection/example/NotRecommendFieldInjectionApplicationTests.java
@@ -0,0 +1,16 @@
+package not.recommend.field.injection.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotRecommendFieldInjectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
