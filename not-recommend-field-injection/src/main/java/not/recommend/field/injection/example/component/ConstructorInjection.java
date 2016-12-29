903a1400aa15e083b8140f254d309131fd3e96f9
pppurple <shuggie.otis0@gmail.com>
2016-12-29 19:37:10 +0900

    update

--- not-recommend-field-injection/src/main/java/not/recommend/field/injection/example/component/ConstructorInjection.java
+++ not-recommend-field-injection/src/main/java/not/recommend/field/injection/example/component/ConstructorInjection.java
@@ -3,7 +3,9 @@ package not.recommend.field.injection.example.component;
 import not.recommend.field.injection.example.service.MyServiceA;
 import not.recommend.field.injection.example.service.MyServiceB;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ConstructorInjection {
     private MyServiceA myServiceA;
     private MyServiceB myServiceB;
