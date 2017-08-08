77d1fb72a278e8e63e7cf554bfbb60faad52d122
pppurple <shuggie.otis0@gmail.com>
2017-08-08 20:53:08 +0900

    test

--- transaction-propagation-example/src/main/java/com/example/propagation/transaction/transactionpropagationexample/controller/MyController.java
+++ transaction-propagation-example/src/main/java/com/example/propagation/transaction/transactionpropagationexample/controller/MyController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
+    //
     private final MyServiceA myServiceA;
     private final MyServiceB myServiceB;
 
