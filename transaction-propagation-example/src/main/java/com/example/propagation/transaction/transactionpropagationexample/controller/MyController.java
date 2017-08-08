c36401188dedbb19b1310580178034524e05a56b
kota.anzai <kota.anzai@linecorp.com>
2017-08-08 20:45:58 +0900

    add propagation

--- transaction-propagation-example/src/main/java/com/example/propagation/transaction/transactionpropagationexample/controller/MyController.java
+++ transaction-propagation-example/src/main/java/com/example/propagation/transaction/transactionpropagationexample/controller/MyController.java
@@ -1,17 +1,101 @@
 package com.example.propagation.transaction.transactionpropagationexample.controller;
 
 import com.example.propagation.transaction.transactionpropagationexample.service.MyServiceA;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.propagation.transaction.transactionpropagationexample.service.MyServiceB;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
-    @Autowired
-    private MyServiceA myServiceA;
+    private final MyServiceA myServiceA;
+    private final MyServiceB myServiceB;
 
-    @RequestMapping("/my")
-    public void update() {
-        myServiceA.updateRequired();
+    public MyController(MyServiceA myServiceA, MyServiceB myServiceB) {
+        this.myServiceA = myServiceA;
+        this.myServiceB = myServiceB;
+    }
+
+    @RequestMapping("/required")
+    public void required() {
+        myServiceA.initDB();
+        myServiceA.required();
+    }
+
+    @RequestMapping("/required_from_b")
+    public void requiredFromB() {
+        myServiceB.initDB();
+        myServiceB.required();
+    }
+
+    @RequestMapping("/requires_new")
+    public void requiresNew() {
+        myServiceA.initDB();
+        myServiceA.requiresNew();
+    }
+
+    @RequestMapping("/requires_new_from_b")
+    public void requiresNewFromB() {
+        myServiceB.initDB();
+        myServiceB.requiresNew();
+    }
+
+    @RequestMapping("/supports")
+    public void supports() {
+        myServiceA.initDB();
+        myServiceA.supports();
+    }
+
+    @RequestMapping("/supports_form_b")
+    public void supportsFromB() {
+        myServiceB.initDB();
+        myServiceB.supports();
+    }
+
+    @RequestMapping("/not_supported")
+    public void notSupported() {
+        myServiceA.initDB();
+        myServiceA.notSupported();
+    }
+
+    @RequestMapping("/not_supported_from_b")
+    public void notSupportedFromB() {
+        myServiceB.initDB();
+        myServiceB.notSupported();
+    }
+
+    @RequestMapping("/mandatory")
+    public void mandatory() {
+        myServiceA.initDB();
+        myServiceA.mandatory();
+    }
+
+    @RequestMapping("/mandatory_from_b")
+    public void mandatoryFromB() {
+        myServiceB.initDB();
+        myServiceB.mandatory();
+    }
+
+    @RequestMapping("/nested")
+    public void nested() {
+        myServiceA.initDB();
+        myServiceA.nested();
+    }
+
+    @RequestMapping("/nested_from_b")
+    public void nestedFromB() {
+        myServiceB.initDB();
+        myServiceB.nested();
+    }
+
+    @RequestMapping("/never")
+    public void never() {
+        myServiceA.initDB();
+        myServiceA.never();
+    }
+
+    @RequestMapping("/never_from_b")
+    public void neverFromB() {
+        myServiceB.initDB();
+        myServiceB.never();
     }
 }
--- transaction-propagation-example/src/main/java/com/example/propagation/transaction/transactionpropagationexample/service/MyServiceB.java
+++ transaction-propagation-example/src/main/java/com/example/propagation/transaction/transactionpropagationexample/service/MyServiceB.java
@@ -1,7 +1,66 @@
 package com.example.propagation.transaction.transactionpropagationexample.service;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MyServiceB {
+    private final JdbcTemplate jdbc;
+    private final MyServiceA myServiceA;
+
+    public MyServiceB(JdbcTemplate jdbc, MyServiceA myServiceA) {
+        this.jdbc = jdbc;
+        this.myServiceA = myServiceA;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void required() {
+        jdbc.update("UPDATE person SET age=? WHERE name = 'Andy'", 20);
+        myServiceA.required();
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void requiresNew() {
+        jdbc.update("UPDATE person SET age=? WHERE name = 'Andy'", 20);
+        myServiceA.requiresNew();
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void supports() {
+        jdbc.update("UPDATE person SET age=? WHERE name = 'Andy'", 20);
+        myServiceA.supports();
+    }
+
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    public void notSupported() {
+        jdbc.update("UPDATE person SET age=? WHERE name = 'Andy'", 20);
+        myServiceA.notSupported();
+    }
+
+    @Transactional
+    public void mandatory() {
+        jdbc.update("UPDATE person SET age=? WHERE name = 'Andy'", 20);
+        myServiceA.mandatory();
+    }
+
+    @Transactional(propagation = Propagation.NESTED)
+    public void nested() {
+        jdbc.update("UPDATE person SET age=? WHERE name = 'Andy'", 20);
+        myServiceA.nested();
+    }
+
+    @Transactional(propagation = Propagation.NEVER)
+    public void never() {
+        jdbc.update("UPDATE person SET age=? WHERE name = 'Andy'", 20);
+        myServiceA.never();
+    }
+
+    public void initDB() {
+        jdbc.execute("DROP TABLE IF EXISTS person");
+        jdbc.execute("CREATE TABLE person (id INTEGER NOT NULL IDENTITY, name VARCHAR(256), age INTEGER)"
+        );
+        jdbc.update("INSERT INTO person (name, age) VALUES (?, ?)", "Andy", 19);
+    }
 }
