835bb32cee2232a92b3f9eb97c3854780423eb7d
pppurple <shuggie.otis0@gmail.com>
2017-10-11 21:54:36 +0900

    add skelton

---
+++ spring-data-redis-example/src/main/java/com/example/redis/data/spring/springdataredisexample/SpringDataRedisExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.redis.data.spring.springdataredisexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRedisExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRedisExampleApplication.class, args);
+	}
+}
