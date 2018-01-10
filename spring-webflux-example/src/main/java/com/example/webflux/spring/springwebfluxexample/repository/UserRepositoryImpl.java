907a29405db0b472afc2f88f119502d68d76cf05
pppurple <shuggie.otis0@gmail.com>
2018-01-10 19:38:12 +0900

    Add log

--- spring-webflux-example/src/main/java/com/example/webflux/spring/springwebfluxexample/repository/UserRepositoryImpl.java
+++ spring-webflux-example/src/main/java/com/example/webflux/spring/springwebfluxexample/repository/UserRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.example.webflux.spring.springwebfluxexample.repository;
 
 import com.example.webflux.spring.springwebfluxexample.controller.WebfluxUserController.User;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -8,6 +9,7 @@ import reactor.core.publisher.Mono;
 import javax.annotation.PostConstruct;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.IntStream;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
@@ -15,19 +17,26 @@ public class UserRepositoryImpl implements UserRepository {
 
     @PostConstruct
     public void init() {
-        users.put(1L, new User(1, "Alice", 20));
+/*        users.put(1L, new User(1, "Alice", 20));
         users.put(2L, new User(2, "Bobby", 32));
-        users.put(3L, new User(3, "Cindy", 41));
+        users.put(3L, new User(3, "Cindy", 41));*/
+
+        IntStream.range(1, 1000)
+                .forEach(i -> {
+                    String name = RandomStringUtils.randomAlphabetic(5);
+                    User u = new User(i, name,  i * 2);
+                    users.put((long)i, u);
+                });
     }
 
     @Override
     public Mono<User> getById(long id) {
-        return Mono.just(users.get(id));
+        return Mono.just(users.get(id)).log();
     }
 
     @Override
     public Flux<User> getAll() {
-        return Flux.fromIterable(users.values());
+        return Flux.fromIterable(users.values()).log();
     }
 
     @Override
