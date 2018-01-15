88fd2ae061c9ab125d7f08aa4bf3aca47b1ffda6
pppurple <shuggie.otis0@gmail.com>
2018-01-15 21:49:59 +0900

    Add log

--- spring-webflux-example/src/main/java/com/example/webflux/spring/springwebfluxexample/repository/UserRepositoryImpl.java
+++ spring-webflux-example/src/main/java/com/example/webflux/spring/springwebfluxexample/repository/UserRepositoryImpl.java
@@ -17,16 +17,16 @@ public class UserRepositoryImpl implements UserRepository {
 
     @PostConstruct
     public void init() {
-/*        users.put(1L, new User(1, "Alice", 20));
+        users.put(1L, new User(1, "Alice", 20));
         users.put(2L, new User(2, "Bobby", 32));
-        users.put(3L, new User(3, "Cindy", 41));*/
+        users.put(3L, new User(3, "Cindy", 41));
 
-        IntStream.range(1, 1000)
+/*        IntStream.range(1, 1000)
                 .forEach(i -> {
                     String name = RandomStringUtils.randomAlphabetic(5);
                     User u = new User(i, name,  i * 2);
                     users.put((long)i, u);
-                });
+                });*/
     }
 
     @Override
@@ -46,7 +46,7 @@ public class UserRepositoryImpl implements UserRepository {
         return user.map(u -> {
             users.put(u.getId(), u);
             return u;
-        }).then();
+        }).log().then();
     }
 
     @Override
@@ -55,12 +55,12 @@ public class UserRepositoryImpl implements UserRepository {
         return users.map(u -> {
             this.users.put(u.getId(), u);
             return u;
-        }).then();
+        }).log().then();
     }
 
     @Override
     public Mono<User> update(long id, Mono<User> user) {
-        return user.doOnNext(u -> users.put(id, u));
+        return user.doOnNext(u -> users.put(id, u)).log();
     }
 
     @Override
