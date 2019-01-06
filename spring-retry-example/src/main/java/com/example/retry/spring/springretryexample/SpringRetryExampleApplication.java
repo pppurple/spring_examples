727c1c535f0db1cdad20221f7c88e6bd1d90ca79
purple <shuggie.otis0@gmail.com>
2019-01-07 02:11:53 +0900

    add retryable annotation examples

--- spring-retry-example/src/main/java/com/example/retry/spring/springretryexample/SpringRetryExampleApplication.java
+++ spring-retry-example/src/main/java/com/example/retry/spring/springretryexample/SpringRetryExampleApplication.java
@@ -1,14 +1,35 @@
 package com.example.retry.spring.springretryexample;
 
+import com.example.retry.spring.springretryexample.service.MyService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
-public class SpringRetryExampleApplication {
+public class SpringRetryExampleApplication implements CommandLineRunner {
+    private final MyService myService;
 
     public static void main(String[] args) {
-		SpringApplication.run(SpringRetryExampleApplication.class, args);
-	}
+        SpringApplication.run(SpringRetryExampleApplication.class, args);
+    }
 
+    public SpringRetryExampleApplication(MyService myService) {
+        this.myService = myService;
+    }
+
+    @Override
+    public void run(String... args) {
+        try {
+            myService.retryable();
+            myService.retryableWithRetryPolicy();
+            myService.retryableWithBackoffPolicy();
+            myService.retryableWithMaxDelay();
+            myService.retryableWithException("test");
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
 }
 
---
+++ spring-retry-example/src/main/java/com/example/retry/spring/springretryexample/service/MyService.java
@@ -0,0 +1,69 @@
+package com.example.retry.spring.springretryexample.service;
+
+import lombok.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class MyService {
+    private int retryCount = 0;
+
+    @Retryable
+    public void retryable() {
+        System.out.println(LocalDateTime.now() + " retry=" + retryCount);
+        retryCount++;
+
+        throw new RuntimeException();
+    }
+
+    @Retryable(maxAttempts = 5)
+    public void retryableWithRetryPolicy() {
+        System.out.println(LocalDateTime.now() + " retry=" + retryCount);
+        retryCount++;
+
+        throw new RuntimeException();
+    }
+
+    @Retryable(backoff = @Backoff(delay = 2_000))
+    public void retryableWithBackoffPolicy() {
+        System.out.println(LocalDateTime.now() + " retry=" + retryCount);
+        retryCount++;
+
+        throw new RuntimeException();
+    }
+
+    @Retryable(backoff = @Backoff(delay = 2_000, maxDelay = 4_000))
+    public void retryableWithMaxDelay() {
+        System.out.println(LocalDateTime.now() + " retry=" + retryCount);
+        retryCount++;
+
+        throw new RuntimeException();
+    }
+
+    @Retryable(UserNotFoundException.class)
+    public void retryableWithException(String name) throws UserNotFoundException {
+        System.out.println(LocalDateTime.now() + " retry=" + retryCount + ", name=" + name);
+        retryCount++;
+
+        throw new UserNotFoundException();
+    }
+
+    @Recover
+    public void recover(UserNotFoundException e, String name) {
+        e.printStackTrace();
+        System.out.println("recovered! name=" + name);
+    }
+
+    @Value
+    public static class User {
+        private String name;
+        private int age;
+    }
+
+    public static class UserNotFoundException extends Exception {
+    }
+}
