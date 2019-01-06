e7efee655bc2f7d07af975a370de8902134a3cee
purple <shuggie.otis0@gmail.com>
2019-01-06 19:46:44 +0900

    add initial

---
+++ spring-retry-example/src/main/java/com/example/retry/spring/springretryexample/SpringRetryExampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.retry.spring.springretryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRetryExampleApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(SpringRetryExampleApplication.class, args);
+	}
+
+}
+
---
+++ spring-retry-example/src/test/java/com/example/retry/spring/springretryexample/SpringRetryExampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.retry.spring.springretryexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRetryExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
